dcec38a8597cd21d22cdb886fc631373bc547203
Manuel Leitold <manuel.leitold@edu.fh-joanneum.at>
2020-04-20 21:02:47 +0200

    use logger

--- node/apps/api/src/main.ts
+++ node/apps/api/src/main.ts
@@ -1,19 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
   await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/', 'bootstrap');
   });
 }
 
