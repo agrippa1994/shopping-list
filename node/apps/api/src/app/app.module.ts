9dd36afc4df025237c2da246f53dae1aac99c317
Manuel Leitold <manuel.leitold@edu.fh-joanneum.at>
2020-04-20 20:57:58 +0200

    remove dead code

--- node/apps/api/src/app/app.module.ts
+++ node/apps/api/src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DataModule } from './data';
 import { GraphqlModule } from './graphql';
 
 @Module({
   imports: [DataModule, GraphqlModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
